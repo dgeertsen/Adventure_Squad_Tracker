2f5b0290be0c28c0c0f71b78855c03ab5c91a8fe
Devon Geertsen <devon.geertsen@gmail.com>
2023-03-24 10:39:48 -0600

    Update adventure_squad_insert_script.sql
    Update insert statement to include my friends and 3 adventures

--- adventure_squad_insert_script.sql
+++ adventure_squad_insert_script.sql
@@ -1,21 +1,28 @@
 INSERT INTO location (city, state_province, country) VALUES
-('New York', 'New York', 'USA'),
-('London', 'Greater London', 'UK'),
-('Sydney', 'New South Wales', 'Australia');
+('Banff', 'Alberta', 'Canada'),
+('Glacier', 'Montana', 'USA'),
+('Lake Como', 'Montana', 'USA');
 
 INSERT INTO adventure (location_id, start_date, end_date) VALUES
-(1, '2023-05-15', '2023-05-22'),
-(2, '2023-06-01', '2023-06-08'),
-(3, '2023-07-10', '2023-07-17');
+(1, '2019-08-15', '2019-08-22'),
+(2, '2023-05-19', '2023-05-23'),
+(3, '2022-07-10', '2023-07-14');
 
 INSERT INTO user (first_name, last_name, age) VALUES
-('John', 'Doe', 30),
-('Jane', 'Smith', 25),
-('Bob', 'Johnson', 40);
+('Devon', 'Geertsen', 32),
+('Mikaela', 'Hayes', 28),
+('Tierra', 'Starr', 29),
+('Kim', 'Lovitt', 27),
+('Michaela', 'Shifley', 40);
 
 INSERT INTO user_adventure (user_id, adventure_id) VALUES
 (1, 1),
-(2, 2),
-(3, 3),
 (1, 2),
-(2, 3);
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 1),
+(3, 3),
+(4, 3),
+(5, 1),
+(5, 2);
